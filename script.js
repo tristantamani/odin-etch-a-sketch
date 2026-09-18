6485ce41252cc692edd94785c5a16bbecb46849b
tristantamani <tristanandreitamani@gmail.com>
2026-09-18 11:11:38 +0800

    Create 16x16 grid of square divs

---
+++ script.js
@@ -0,0 +1,17 @@
+function createGrid() {
+    for (let i = 0; i < 16; i++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+        div.appendChild(row);
+
+        for (let j = 0; j < 16; j++) {
+            const square = document.createElement("div");
+            square.classList.add("square");
+            row.appendChild(square);
+        }
+    }
+}
+
+const div = document.querySelector("#container");
+
+createGrid();
